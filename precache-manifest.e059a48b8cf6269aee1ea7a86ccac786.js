c2ba4a622cc9c075c3734a1755ad160ea0a125be
Nastya <sunflower10019@gmail.com>
2020-08-03 13:45:02 +0400

    Updates

---
+++ precache-manifest.e059a48b8cf6269aee1ea7a86ccac786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b568eb03b537d359b081b073ace1391",
+    "url": "/lunar10019/react-project.git/index.html"
+  },
+  {
+    "revision": "22dd47bd7074f198a0b9",
+    "url": "/lunar10019/react-project.git/static/css/main.48bc95b9.chunk.css"
+  },
+  {
+    "revision": "ba26cd2bf484411e5f59",
+    "url": "/lunar10019/react-project.git/static/js/2.fe6aa84a.chunk.js"
+  },
+  {
+    "revision": "276d605f06b39ad72d3daa4025273953",
+    "url": "/lunar10019/react-project.git/static/js/2.fe6aa84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dd47bd7074f198a0b9",
+    "url": "/lunar10019/react-project.git/static/js/main.d7c9c6b8.chunk.js"
+  },
+  {
+    "revision": "6e216961df7d41b51057",
+    "url": "/lunar10019/react-project.git/static/js/runtime-main.528db14b.js"
+  }
+]);
